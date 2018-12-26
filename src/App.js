cdb1d791b92919aed172c0123efbad47c01f63d0
Jack Cheng <jack.cheng@ballvenie.com>
2018-12-26 17:21:58 +0800

    try pwa install prompt

--- src/App.js
+++ src/App.js
@@ -12,19 +12,58 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      games: []
+      games: [],
+      installButton: true
     };
   }
 
   componentDidMount() {
+    window.addEventListener("beforeinstallprompt", e => {
+      // For older browsers
+      e.preventDefault();
+      this.installPrompt = e;
+      // See if the app is already installed, in that case, do nothing
+      if (
+        (window.matchMedia &&
+          window.matchMedia("(display-mode: standalone)").matches) ||
+        window.navigator.standalone === true
+      ) {
+        return false;
+      }
+      // Set the state variable to make button visible
+      this.setState({
+        installButton: true
+      });
+    });
     Axios.get("https://hidden-lowlands-59931.herokuapp.com/games").then(res => {
       this.setState({
         games: res.data
       });
     });
   }
+
+  installPrompt = null;
+
+  installApp = async () => {
+    console.log(this.installPrompt);
+    if (!this.installPrompt) return false;
+    this.installPrompt.prompt();
+    let outcome = await this.installPrompt.userChoice;
+    if (outcome.outcome === "accepted") {
+      console.log("App Installed");
+    } else {
+      console.log("App not installed");
+    }
+    // Remove the event reference
+    this.installPrompt = null;
+    // Hide the button
+    this.setState({
+      installButton: false
+    });
+  };
+
   render() {
-    const { games } = this.state;
+    const { games, installButton } = this.state;
     return (
       <div className="App">
         <GameContext.Provider value={this.state.games}>
@@ -33,6 +72,12 @@ class App extends Component {
             <GameDetail path="/game/:id" games={games} />
           </Router>
         </GameContext.Provider>
+        {installButton && (
+          <div className="install-btn" onClick={this.installApp}>
+            <div className="overlay" />
+            <button>安裝</button>
+          </div>
+        )}
       </div>
     );
   }
